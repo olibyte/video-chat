3126dcc5fe1ee79afbd0856371d06136dd63afda
Oliver <ocben1@gmail.com>
2021-03-06 10:07:15 +1100

    add firebase

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Navigation />
+    <router-view />
+  </div>
+</template>
+<script>
+import Navigation from '@/components/Navigation'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+<style lang="scss">
+$primary: #5f2882;
+@import 'node_modules/bootstrap/scss/bootstrap';
+</style>
