ccda91c7f2cdf426b89f5792dcef72cfdc09b793
Oliver <ocben1@gmail.com>
2021-04-03 11:57:30 +1100

    adding icons and deleting rrecords

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash, faPodcast, faVideo, faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash, faPodcast, faVideo, faUser)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -41,6 +41,24 @@
           </div>
           <div class="list-group list-group-flush">
             <div class="list-group-item d-flex" v-for="item in rooms" :key="item.id">
+                <section class="btn-group align-self-center" role="group" aria-label="Room Options">
+  <button
+    class="btn btn-sm btn-outline-secondary"
+    title="Delete Room"
+    @click="$emit('deleteRoom', item.id)"
+  >
+    <font-awesome-icon icon="trash"></font-awesome-icon>
+  </button>
+
+  <router-link class="btn btn-sm btn-outline-secondary" title="Check In" to="/">
+      <font-awesome-icon icon="user"></font-awesome-icon>
+      </router-link>
+
+  <router-link class="btn btn-sm btn-outline-secondary" title="Attendees" to="/">
+        <font-awesome-icon icon="video"></font-awesome-icon>
+  </router-link>
+</section>
+
               <section class="pl-3 text-left align-self-center">{{ item.name }}</section>
             </div>
           </div>
@@ -50,12 +68,16 @@
   </div>
 </template>
 <script>
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 export default {
   name: 'Rooms',
   data: function () {
     return {
       roomName: null
     }
+  },
+  components: {
+      FontAwesomeIcon
   },
   methods: {
     handleAdd: function () {
